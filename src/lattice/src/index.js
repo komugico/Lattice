3abcc8f44e8950c8a869e4fdcd4b5fb0fa9d5e4b
komugico <shu.isaka.pc+develop@gmail.com>
2021-05-05 13:02:07 +0900

    不要なデータを消去

--- src/lattice/src/index.js
+++ src/lattice/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import { store } from './app/store';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
